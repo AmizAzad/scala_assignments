cadc7d346426cc6900ca60e5ca2dcf9cb2808877
Amiz Azad <a.azad@adform.com>
2024-07-02 13:05:31 +0530

    Assignment 01 - Exam data problem

---
+++ exam_data_problem/Examdata.scala
@@ -0,0 +1,20 @@
+package exam_data_problem
+
+import scala.io.Source
+
+object Examdata {
+  def main(args: Array[String]): Unit = {
+
+    if (args.length != 1) {
+      println("Please provide an input exam data csv file to process")
+      sys.exit()
+    }
+    println(s"File to process: ${args(0)}")
+
+    Source.fromFile(args(0)).getLines().toList.foreach(line => {
+      val element: Array[Int] = line.split(",").map(_.trim.toInt)
+      val timeForPrep = element(0) * element(1)
+      if (timeForPrep <= element(2)) println("YES") else println("NO")
+    })
+  }
+}
